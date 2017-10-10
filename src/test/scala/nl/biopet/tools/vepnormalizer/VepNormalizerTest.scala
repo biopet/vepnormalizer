5c67618fc171cc8bf24049b60bb37ed49f4c5989
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-10 21:22:25 +0200

    Added template to all tools

---
+++ src/test/scala/nl/biopet/tools/vepnormalizer/VepNormalizerTest.scala
@@ -0,0 +1,13 @@
+package nl.biopet.tools.vepnormalizer
+
+import nl.biopet.test.BiopetTest
+import org.testng.annotations.Test
+
+object VepNormalizerTest extends BiopetTest {
+  @Test
+  def testNoArgs(): Unit = {
+    intercept[IllegalArgumentException] {
+      ToolTemplate.main(Array())
+    }
+  }
+}
