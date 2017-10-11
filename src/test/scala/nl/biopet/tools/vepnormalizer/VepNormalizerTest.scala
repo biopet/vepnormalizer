d4b29a41f700af5e15154f55c2174b0897f4cdae
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:50:37 +0200

    object to class

--- src/test/scala/nl/biopet/tools/vepnormalizer/VepNormalizerTest.scala
+++ src/test/scala/nl/biopet/tools/vepnormalizer/VepNormalizerTest.scala
@@ -3,7 +3,7 @@ package nl.biopet.tools.vepnormalizer
 import nl.biopet.test.BiopetTest
 import org.testng.annotations.Test
 
-object VepNormalizerTest extends BiopetTest {
+class VepNormalizerTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
